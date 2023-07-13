b403e338323dd6d0bf73280c1b483b35fc9f5477
Felipe Nóbrega <felipenobrega2012@gmail.com>
2023-07-13 17:02:52 -0300

    styling header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
+import { Transaction } from "./pages/Transaction";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Hello World</h1>
+      <GlobalStyle />
+      <Transaction />
     </ThemeProvider>
-  )
+  );
 }
