b239fc4ad22b7d4596fd6d320168736a54f13a3e
Felipe Nóbrega <felipenobrega2012@gmail.com>
2023-07-17 16:15:40 -0300

    creating transactions context

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
 import { Transaction } from "./pages/Transaction";
+import { TransactionsProvider } from "./contexts/TransactionContext";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
+      <TransactionsProvider>
       <Transaction />
+      </TransactionsProvider>
     </ThemeProvider>
   );
 }
--- src/pages/Transaction/index.tsx
+++ src/pages/Transaction/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useContext } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
@@ -7,30 +7,10 @@ import {
   TransactionsContainer,
   TransactionsTable,
 } from "./styles";
-
-interface Transaction {
-  id: number
-  description: string
-  type: 'income' | 'outcome'
-  price: number
-  category: string
-  createdAt: string
-}
+import { TransactionsContext } from "../../contexts/TransactionContext";
 
 export function Transaction() {
-  const [transaction, setTransaction] = useState<Transaction[]>([])
-
-  async function loadTransaction() {
-   const response = await fetch("http://localhost:3000/transactions")
-   const data = await response.json()
-
-   setTransaction(data)
-  } 
-
-  useEffect(() => {
-    loadTransaction()
-  }, [])
-
+   const { transaction } = useContext(TransactionsContext)
   return (
     <div>
       <Header />
@@ -40,19 +20,21 @@ export function Transaction() {
         <SearchForm />
         <TransactionsTable>
           <tbody>
-            {transaction.map(transaction => {
+            {transaction.map((transaction) => {
               return (
                 <tr key={transaction.id}>
-                <td width="50%">{transaction.description}</td>
-                <td>
-                  <PriceHighlight variant={transaction.type}>{transaction.price}</PriceHighlight>
-                </td>
-                <td>{transaction.category}</td>
-                <td>{transaction.createdAt}</td>
-              </tr>
-              )
+                  <td width="50%">{transaction.description}</td>
+                  <td>
+                    <PriceHighlight variant={transaction.type}>
+                      {transaction.price}
+                    </PriceHighlight>
+                  </td>
+                  <td>{transaction.category}</td>
+                  <td>{transaction.createdAt}</td>
+                </tr>
+              );
             })}
-            </tbody>
+          </tbody>
         </TransactionsTable>
       </TransactionsContainer>
     </div>
